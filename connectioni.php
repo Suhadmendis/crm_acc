168637257f75ae0b13d826fff563886c81fafc71
Suhadmendis <suhad.a.mendis@gmail.com>
2019-11-21 16:27:42 +0530

    del

--- connectioni.php
+++
@@ -1,9 +0,0 @@
-<?php
-$GLOBALS['hostname'] = 'localhost';
-$GLOBALS['username'] = 'root';
-$GLOBALS['password'] = '';
-$GLOBALS['db'] = 'wingslog_19';
- 
-$GLOBALS['dbinv'] = mysqli_connect($GLOBALS['hostname'],$GLOBALS['username'],$GLOBALS['password'],$GLOBALS['db']);
- 
-?>
